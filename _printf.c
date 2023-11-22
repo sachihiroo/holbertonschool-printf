6d0295043c6536eb9dc080f30994099a4ac56f6f
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-22 16:09:54 +0100

    ok jawk bahi

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 		{'s', string_print},
 		{'\0', NULL},
 	};
-	unsigned int i, j, size = 0;
+	unsigned int i, j, size=0;
 	va_list args;
 
 	va_start(args, format);
@@ -24,9 +24,9 @@ int _printf(const char *format, ...)
 		{
 			_putchar(format[i]);
 		}
-		if (format[i + 1] == f[j].str)
+		for (j = 0; f[j].str < '\0'; j++)
 		{
-			for (j = 0; f[j].str < '\0'; j++)
+			if (format[i + 1] == f[j].str)
 			{
 				f[j].print(&args);
 			}

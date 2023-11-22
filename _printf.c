64047637723c1b3731b9170c4c60ca69788d9b05
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-22 23:09:19 +0100

    ok jawk bahi

--- _printf.c
+++ _printf.c
@@ -29,13 +29,13 @@ int _printf(const char *format, ...)
 		{
 			for (j = 0; f[j].str <= '\0'; j++)
 			{
-				if (format[i+1] == f[j].str)
+				if (format[i + 1] == f[j].str)
 				{
-					i++;
 					f[j].print(&args);
+					size += string_length(args);
+					i+=1;
 					break;
 				}
-			size += string_length(args);
 			}
 		}
 	}

75678f9626c60224f6307195fcb6428114ea3ff8
sachihiroo <saif3antar@gmail.com>
2023-11-22 10:50:17 +0100

    printf funct

---
+++ printf_function.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void char_print(va_list *args)
+{
+	_putchar(va_arg(*args, int));
+}
+/**
+ *
+ *
+ *
+ */
+void pert_print(va_list *args)
+{
+	_putchar('%');
+}
+/**
+ *
+ *
+ *
+ */
+void string_print(va_list *args)
+{
+	str(va_arg(*args, char *));
+}

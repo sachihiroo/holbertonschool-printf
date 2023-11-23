7db54ef74c02a0c8d002ba4f180811f53db6b263
sachihiroo <saif3antar@gmail.com>
2023-11-23 06:36:35 +0100

    Func print

--- printf_function.c
+++ printf_function.c
@@ -1,26 +1,23 @@
 #include "main.h"
 /**
- *
- *
- *
+ * char_print - check the code
+ * @args: pointer
  */
 void char_print(va_list *args)
 {
 	_putchar(va_arg(*args, int));
 }
 /**
- *
- *
- *
+ * pert_print - check the code
+ * @args: pointer
  */
-void pert_print()
+void pert_print(void)
 {
 	_putchar('%');
 }
 /**
- *
- *
- *
+ * string_print
+ * @args: pointer
  */
 void string_print(va_list *args)
 {

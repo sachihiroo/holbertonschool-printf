6003c6430dda72c98b14d5354d6fcc8ace1cba3d
sachihiroo <saif3antar@gmail.com>
2023-11-27 10:53:05 +0100

    convert to binary

--- Binary.c
+++ Binary.c
@@ -7,14 +7,14 @@
 int binary(va_list args)
 {
 	int dic_num, bin_num = 0, i = 1, reminder;
-	int s=1;
+	int s = 0;
 
 	dic_num = va_arg(args, int);
 	while (dic_num != 0)
 	{
 		reminder = dic_num % 2;
 		dic_num /= 2;
-		s +=_putchar(bin_num += reminder * i);
+		s += _putchar(bin_num += reminder * i);
 		i *= 10;
 	}
 	return (s);
